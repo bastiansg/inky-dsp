6c4b9dc0c8ae1e8f55944dbac0d0acfa51ded375
Bas <bastiansg.bas@gmail.com>
2026-05-06 01:40:38 -0300

    async callback function at InkyButtons

--- src/inky_dsp/buttons/buttons.py
+++ src/inky_dsp/buttons/buttons.py
@@ -4,12 +4,10 @@ import gpiodevice
 from datetime import timedelta
 from threading import Lock
 from rich.console import Console
-from typing import Callable, Self
+from typing import Awaitable, Callable, Self
 
 from pydantic import BaseModel, StrictInt, StrictStr, model_validator
 from gpiod.line import Bias, Direction, Edge
-
-from .utils import threaded
 
 
 console = Console()
@@ -37,7 +35,7 @@ class InkyButtons:
     def __init__(
         self,
         button_map: ButtonMap,
-        callback: Callable[[ButtonEvent], None],
+        callback: Callable[[ButtonEvent], Awaitable[None]],
         debounce_period_ms: int = 100,
     ):
         self.button_map = button_map
@@ -47,50 +45,35 @@ class InkyButtons:
         self.start_lock = Lock()
         self.is_running: bool = False
 
-    @threaded
-    def start(self) -> None:
-        if not self.start_lock.acquire(blocking=False):
-            return
+    # @threaded
+    async def run(self) -> None:
+        _input = gpiod.LineSettings(
+            direction=Direction.INPUT,
+            bias=Bias.PULL_UP,
+            edge_detection=Edge.FALLING,
+            debounce_period=timedelta(milliseconds=self.debounce_period_ms),
+        )
 
-        console.log("InkyButtons started.")
-        try:
-            _input = gpiod.LineSettings(
-                direction=Direction.INPUT,
-                bias=Bias.PULL_UP,
-                edge_detection=Edge.FALLING,
-                debounce_period=timedelta(milliseconds=self.debounce_period_ms),
-            )
+        chip = gpiodevice.find_chip_by_platform()
+        offsets = [
+            chip.line_offset_from_id(pin) for pin in self.button_map.buttons
+        ]
 
-            chip = gpiodevice.find_chip_by_platform()
-            offsets = [
-                chip.line_offset_from_id(pin) for pin in self.button_map.buttons
-            ]
+        request = chip.request_lines(
+            consumer="inky-dsp-buttons",
+            config=dict.fromkeys(offsets, _input),
+        )
 
-            request = chip.request_lines(
-                consumer="inky-dsp-buttons",
-                config=dict.fromkeys(offsets, _input),
-            )
-
-            self.is_running = True
-            while self.is_running:
-                for event in request.read_edge_events():
-                    if not self.is_running:
-                        break
-
-                    index = offsets.index(event.line_offset)
-                    self.callback(
-                        ButtonEvent(
-                            label=self.button_map.labels[index],
-                            gpio=self.button_map.buttons[index],
-                            index=index,
-                        )
+        while True:
+            for event in request.read_edge_events():
+                index = offsets.index(event.line_offset)
+                await self.callback(
+                    ButtonEvent(
+                        label=self.button_map.labels[index],
+                        gpio=self.button_map.buttons[index],
+                        index=index,
                     )
+                )
 
-                    while request.wait_edge_events(timeout=timedelta()):
-                        request.read_edge_events()
-
-        finally:
-            self.start_lock.release()
-
-    def stop(self) -> None:
-        self.is_running = False
+                while request.wait_edge_events(timeout=timedelta()):
+                    request.read_edge_events()
